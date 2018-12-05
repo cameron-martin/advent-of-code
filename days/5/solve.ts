303d2c7f00871aaf88dad11a944e04d32fdc05b2
Cameron Martin <cameronmartin123@gmail.com>
2018-12-05 20:40:48 +0000

    Day 5 solution

---
+++ days/5/solve.ts
@@ -0,0 +1,49 @@
+import fs from 'fs-extra';
+import path from 'path';
+
+(async () => {
+    const input = (await fs.readFile(path.resolve(__dirname, 'input.txt'), { encoding: 'utf8' })).trim();
+
+    console.log(reactPolymer(input).length);
+
+    const polymers = genCharArray('a', 'z').map(char => ({ removedType: char, polymer: reactPolymer(removeType(char, input)) }));
+
+    const lowestLength = minBy(polymers, ({ polymer }) => polymer.length);
+
+    console.log(lowestLength.polymer.length);
+})();
+
+function reactPolymer(polymer: string): string {
+    let output = polymer;
+
+    for(let i = 0; i < output.length - 1; i++) {
+        if(output[i] !== output[i+1] && output[i].toLowerCase() === output[i+1].toLowerCase()) {
+            output = output.slice(0, i) + output.slice(i + 2);
+            i = Math.max(-1, i - 2);
+        }
+    }
+
+    return output;
+}
+
+function removeType(type: string, polymer: string) {
+    let output = '';
+
+    for(let i = 0; i < polymer.length; i++) {
+        if(polymer[i].toLowerCase() !== type) output += polymer[i];
+    }
+
+    return output;
+}
+
+function minBy<T>(array: T[], by: (elem: T) => number): T {
+    return array.reduce((currentLowest, elem) => by(elem) < by(currentLowest) ? elem : currentLowest);
+}
+
+function genCharArray(charA: string, charZ: string) {
+    var a = [], i = charA.charCodeAt(0), j = charZ.charCodeAt(0);
+    for (; i <= j; ++i) {
+        a.push(String.fromCharCode(i));
+    }
+    return a;
+}
