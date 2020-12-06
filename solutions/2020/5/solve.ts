dec302a6731a01bf373d697bb69ea37ab45ad79b
Cameron Martin <cameron.martin@forgerock.com>
2020-12-06 11:50:30 +0000

    Solve 2020 day 5 part 2

--- solutions/2020/5/solve.ts
+++ solutions/2020/5/solve.ts
@@ -1,21 +1,20 @@
-import { count } from "console";
 import { maxBy } from "../../../lib/collections";
 import { puzzle } from "../../../lib/puzzle";
 import { getInputLines } from "../../../lib/user-input";
 
 export default puzzle(async () => {
-    const lines = await getInputLines(__dirname);
+    const seatNumbers = (await getInputLines(__dirname)).map(getSeatNumber);
     
     return {
-        part1: solvePart1(lines),
-        // part2: solvePart2(lines),
+        part1: solvePart1(seatNumbers),
+        part2: solvePart2(seatNumbers),
     };
 });
 
 function getSeatNumber(seat: string) {
     let seatNumber = 0;
 
-    for(let i = 0; i < 9; i++) {
+    for(let i = 0; i < 10; i++) {
         if(seat[i] == 'B' || seat[i] === 'R') {
             seatNumber += Math.pow(2, 9 - i);
         }
@@ -24,6 +23,20 @@ function getSeatNumber(seat: string) {
     return seatNumber;
 }
 
-function solvePart1(lines: string[]) {
-    return maxBy(lines.map(getSeatNumber), x => x);
+function solvePart1(seatNumbers: number[]) {
+    return maxBy(seatNumbers, x => x);
+}
+
+function solvePart2(seatNumbers: number[]): number {
+    const maxSeatNumber = maxBy(seatNumbers, x => x);
+
+    let seatNumbersSet = new Set(seatNumbers);
+
+    for(let i = 1; i < maxSeatNumber; i++) {
+        if(!seatNumbersSet.has(i) && seatNumbersSet.has(i-1) && seatNumbersSet.has(i+1)) {
+            return i;
+        }
+    }
+
+    throw new Error('Could not find seat');
 }
